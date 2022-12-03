55716e2314faf046af1b78fe2eb9d2e6f3863b92
CharlesBrunwald <cbrunwald@hotmail.com>
2022-12-02 21:11:37 -0500

    initial commit

---
+++ WordDisplayer/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WordDisplayer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.NextButton = new System.Windows.Forms.Button();
+			this.WordText = new System.Windows.Forms.Label();
+			this.ConfigurationFileList = new System.Windows.Forms.FlowLayoutPanel();
+			this.ImageBox = new System.Windows.Forms.PictureBox();
+			this.ParseButton = new System.Windows.Forms.Button();
+			this.ParseSearchBox = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.ImageBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// NextButton
+			// 
+			this.NextButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+			this.NextButton.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.NextButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.NextButton.Location = new System.Drawing.Point(386, 569);
+			this.NextButton.Margin = new System.Windows.Forms.Padding(4);
+			this.NextButton.Name = "NextButton";
+			this.NextButton.Size = new System.Drawing.Size(613, 62);
+			this.NextButton.TabIndex = 0;
+			this.NextButton.Text = "Next";
+			this.NextButton.UseVisualStyleBackColor = true;
+			this.NextButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NextButton_MouseClick);
+			// 
+			// WordText
+			// 
+			this.WordText.Anchor = System.Windows.Forms.AnchorStyles.Top;
+			this.WordText.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.WordText.Location = new System.Drawing.Point(284, 47);
+			this.WordText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.WordText.Name = "WordText";
+			this.WordText.Size = new System.Drawing.Size(800, 129);
+			this.WordText.TabIndex = 1;
+			this.WordText.Text = "WORD HERE";
+			this.WordText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// ConfigurationFileList
+			// 
+			this.ConfigurationFileList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.ConfigurationFileList.AutoScroll = true;
+			this.ConfigurationFileList.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ConfigurationFileList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ConfigurationFileList.Location = new System.Drawing.Point(16, 260);
+			this.ConfigurationFileList.Margin = new System.Windows.Forms.Padding(4);
+			this.ConfigurationFileList.Name = "ConfigurationFileList";
+			this.ConfigurationFileList.Size = new System.Drawing.Size(276, 370);
+			this.ConfigurationFileList.TabIndex = 3;
+			// 
+			// ImageBox
+			// 
+			this.ImageBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+			this.ImageBox.InitialImage = null;
+			this.ImageBox.Location = new System.Drawing.Point(386, 237);
+			this.ImageBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.ImageBox.Name = "ImageBox";
+			this.ImageBox.Size = new System.Drawing.Size(613, 299);
+			this.ImageBox.TabIndex = 4;
+			this.ImageBox.TabStop = false;
+			// 
+			// ParseButton
+			// 
+			this.ParseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.ParseButton.BackColor = System.Drawing.Color.Red;
+			this.ParseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ParseButton.Location = new System.Drawing.Point(1129, 47);
+			this.ParseButton.Name = "ParseButton";
+			this.ParseButton.Size = new System.Drawing.Size(142, 61);
+			this.ParseButton.TabIndex = 5;
+			this.ParseButton.Text = "Parse File";
+			this.ParseButton.UseVisualStyleBackColor = false;
+			this.ParseButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ParseButton_MouseClick);
+			// 
+			// ParseSearchBox
+			// 
+			this.ParseSearchBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.ParseSearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ParseSearchBox.Location = new System.Drawing.Point(1091, 11);
+			this.ParseSearchBox.Name = "ParseSearchBox";
+			this.ParseSearchBox.Size = new System.Drawing.Size(221, 30);
+			this.ParseSearchBox.TabIndex = 6;
+			this.ParseSearchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ParseSearchBox_KeyDown);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(1331, 665);
+			this.Controls.Add(this.ParseSearchBox);
+			this.Controls.Add(this.ParseButton);
+			this.Controls.Add(this.ImageBox);
+			this.Controls.Add(this.ConfigurationFileList);
+			this.Controls.Add(this.WordText);
+			this.Controls.Add(this.NextButton);
+			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+			this.Name = "MainForm";
+			this.Text = "Word Picker";
+			((System.ComponentModel.ISupportInitialize)(this.ImageBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button NextButton;
+		private System.Windows.Forms.Label WordText;
+		private System.Windows.Forms.FlowLayoutPanel ConfigurationFileList;
+		private System.Windows.Forms.PictureBox ImageBox;
+		private System.Windows.Forms.Button ParseButton;
+		private System.Windows.Forms.TextBox ParseSearchBox;
+	}
+}
+
---
+++ WordDisplayer/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace WordDisplayer
+{
+	public struct FileInfo
+	{
+		public string word;
+		public string imagePath;
+	}
+
+	public partial class MainForm : Form
+	{
+		const string CONF_FOLDER_NAME = "Configuration";
+		const string IMAGE_FOLDER = "Images";
+
+		FileInfo[] _InfoInCurrentFile = { };
+
+		Random _RandomGenerator = new Random();
+		int _LastNumberPicked = -1;
+
+		public MainForm()
+		{
+			InitializeComponent();
+
+			//this.FormBorderStyle = FormBorderStyle.FixedSingle;
+			//this.MaximizeBox = false;
+
+			ImageBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+			if(!Directory.Exists( IMAGE_FOLDER ) )
+			{
+				Directory.CreateDirectory(IMAGE_FOLDER);
+			}
+			if ( !Directory.Exists( CONF_FOLDER_NAME ) )
+			{
+				Directory.CreateDirectory(CONF_FOLDER_NAME);
+				MessageBox.Show( $"Directory: {CONF_FOLDER_NAME} did not exist. It has been created.", "Warning",
+					MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+				ChangeLabel( $"No files in directory: {CONF_FOLDER_NAME}" );
+				return;
+			}
+			string[] allFiles = Directory.GetFiles(CONF_FOLDER_NAME);
+			for(int i = 0; i < allFiles.Length; i++)
+			{
+				Button button = new Button();
+				button.Text = allFiles[i];
+				button.AutoSize = true;
+				button.Click += FileButton_Click;
+				ConfigurationFileList.Controls.Add(button);
+			}
+
+			ChangeLabel( allFiles.Length == 0 ? $"No files in directory: {CONF_FOLDER_NAME}" : "Pick a file to load.");
+		}
+
+		private void FileButton_Click(object sender, EventArgs e)
+		{
+			Button button = (Button)sender;
+			_InfoInCurrentFile = ConfigurationUtils.ReadConfigurationFile( button.Text, IMAGE_FOLDER );
+			_LastNumberPicked = -1;
+			ChangeLabel(_InfoInCurrentFile.Length == 0 ? $"No words in file{button.Text}..." : null );
+		}
+
+		private void ChangeLabel(string forceLabel = null)
+		{
+			if ( forceLabel != null )
+			{
+				this.WordText.Text = forceLabel;
+				return;
+			}
+			int randomNumber = _RandomGenerator.Next(_InfoInCurrentFile.Length);
+			
+			for (int i = 0; randomNumber == _LastNumberPicked; i++ )
+			{
+				if (i == 100) break;
+				randomNumber = _RandomGenerator.Next(_InfoInCurrentFile.Length);
+			}
+			_LastNumberPicked = randomNumber;
+			
+
+			if (_InfoInCurrentFile.Length > 0 )
+			{
+				this.WordText.Text = _InfoInCurrentFile[randomNumber].word;
+				ChangeImage(_InfoInCurrentFile[randomNumber].imagePath);
+			}
+			else
+			{
+				this.WordText.Text = "No words in this file.";
+				ImageBox.Visible = false;
+			}
+		}
+
+		private void ChangeImage( string imagePath )
+		{
+			if( imagePath != "" && File.Exists(imagePath) )
+			{
+				ImageBox.Visible = true;
+				ImageBox.Image = System.Drawing.Image.FromFile(imagePath);
+			}
+			else
+			{
+				ImageBox.Visible = false;
+			}
+		}
+
+		private void NextButton_MouseClick(object sender, MouseEventArgs e)
+		{
+			ChangeLabel();
+		}
+
+		private void ParseButton_MouseClick(object sender, MouseEventArgs e)
+		{
+			string filePath = CONF_FOLDER_NAME + '/' +  ParseSearchBox.Text;
+			if( ConfigurationUtils.CleanFile(filePath) )
+			{
+				MessageBox.Show($"{filePath} has been succefully parsed.", "Success" );
+			}
+		}
+
+		private void ParseSearchBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if( e.KeyCode == Keys.Enter )
+			{
+				ParseButton_MouseClick(null, null);
+			}
+		}
+	}
+
+	class ConfigurationUtils
+	{
+		public const char COMMENT = '#';
+		public const char WORD_IMAGE_SEPERATOR = ',';
+		public const string DONT_PARSE = "#DONT_PARSE";
+
+		public static FileInfo[] ReadConfigurationFile( string FilePath, string ImageFolder )
+		{
+			List<FileInfo> allInfo = new List<FileInfo>();
+			if (File.Exists(FilePath))
+			{
+				if(ImageFolder.LastIndexOf('/') == -1 && ImageFolder.LastIndexOf('\\') == -1 )
+				{
+					ImageFolder += '/';
+				}
+
+				foreach (string line in File.ReadLines(FilePath))
+				{
+					line.Trim();
+					if ( line != "" && line[0] != COMMENT ) // empty or comment
+					{
+						FileInfo fileInfo = new FileInfo();
+						int indexOfComma = line.LastIndexOf(WORD_IMAGE_SEPERATOR);
+						if( indexOfComma != -1 ) // Do we have an image?
+						{
+							fileInfo.word = line.Substring(0, indexOfComma);
+							fileInfo.imagePath = ImageFolder + line.Substring(indexOfComma + 1);
+							fileInfo.imagePath = fileInfo.imagePath.Trim();
+							fileInfo.imagePath = fileInfo.imagePath.Replace( " ", "" );
+						}
+						else
+						{
+							fileInfo.word = line;
+							fileInfo.imagePath = "";
+						}
+						allInfo.Add(fileInfo);
+					}
+				}
+			}
+
+			if (allInfo.Count == 0 )
+			{
+				MessageBox.Show( $"{FilePath} is empty..." );
+			}
+			return allInfo.ToArray();
+		}
+
+		public static bool CleanFile( string filePath )
+		{
+			if(!filePath.Contains( ".txt" ) )
+			{
+				filePath += ".txt";
+			}
+
+			if(!File.Exists(filePath))
+			{
+				MessageBox.Show($"File: {filePath} does not seem to exist.", "ERROR",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return false;
+			}
+
+			string[] allLines = File.ReadAllLines(filePath); // words in line are seperated by \t or ' '
+
+			if (allLines.Length > 0 && allLines[0].Contains(DONT_PARSE))
+			{
+				MessageBox.Show($"{filePath} has the {DONT_PARSE} flag.", $"{DONT_PARSE}", 
+					MessageBoxButtons.OK, MessageBoxIcon.Warning );
+				return false;
+			}
+
+			List<string> parsedLines = new List<string>();
+			foreach(string line in allLines)
+			{
+				if (line == "") continue;
+
+				line.Trim();
+				string tempString = "";
+				for( int j = 0; j < line.Length; j++ )
+				{
+					if( line[j] == '\t' || line[j] == ' ' || j == line.Length - 1 )
+					{
+						if( j == line.Length - 1)
+						{
+							tempString += line[j];
+						}
+						// Remove all numbers
+						float outFloat;
+						if(float.TryParse( tempString, out outFloat ) )
+						{
+							tempString = "";
+							continue;
+						}
+						parsedLines.Add(tempString);
+						tempString = "";
+					}
+					else
+					{
+						tempString += line[j];
+					}
+				}
+			}
+
+			// Clear file
+			File.WriteAllText(filePath, string.Empty);
+			// Write to file with new parsed lines;
+			StreamWriter streamWriter = File.AppendText(filePath);
+			foreach( string line in parsedLines )
+			{
+				if( line != string.Empty )
+				{
+					streamWriter.WriteLine(line);
+				}
+			}
+			streamWriter.Close();
+
+			return true;
+		}
+	}
+}
